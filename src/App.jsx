89fb4ef6a285f7469d18c30e6d0bac00be3c9c81
Jiger2pp <pankaj.pandey.jec@gmail.com>
2025-04-30 22:46:50 +0530

    setup  vite react frontend with navbar, footer, profile and login components using Daisy UI and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Body from "./components/Body";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+
+function App() {  
+
+  return (
+    <> 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />} >
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+                 
+    </>
+  )
+}
+
+export default App
