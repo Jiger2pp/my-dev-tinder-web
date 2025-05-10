9aea9309bf17a8ddee56f41f0b5bf829c1a758af
Jiger2pp <pankaj.pandey.jec@gmail.com>
2025-05-10 13:57:13 +0530

    set socket path

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,11 +2,25 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 const socketConnect = () => {
-    return io(BASE_URL,{
-        auth: {
-            serverOffset: 0
-        }
-    });
+    if(location.hostname === "localhost"){
+
+        return io(BASE_URL,{
+            auth: {
+                serverOffset: 0
+            }
+        });
+
+    }else{
+        return io(BASE_URL,{
+            auth: {
+                serverOffset: 0
+            },
+            path: {
+                "/api/socket.io/"
+            }
+        });
+    }
+    
 };
 
 export default socketConnect;
